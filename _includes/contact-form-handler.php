09e52eda82cb175bea6fc090c23ebe89d2485735
ekanou <ekanoulas@gmail.com>
2016-10-20 10:31:35 +0200

    Update contact-form-handler.php

--- _includes/contact-form-handler.php
+++ _includes/contact-form-handler.php
@@ -1,30 +1,29 @@
 <?php 
 $errors = '';
-$myemail = 'yourname@website.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
+$myemail = 'e.kanoulas@uva.nl';
+if(empty($_POST['name'])  ||
+   empty($_POST['organization'])  ||
    empty($_POST['email']) || 
    empty($_POST['message']))
 {
     $errors .= "\n Error: all fields are required";
 }
-
-$name = $_POST['name']; 
+$name = $_POST['name'];
+$org = $_POST['organization'];
 $email_address = $_POST['email']; 
 $message = $_POST['message']; 
-
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
 $email_address))
 {
     $errors .= "\n Error: Invalid email address";
 }
-
 if( empty($errors))
 {
 	$to = $myemail; 
-	$email_subject = "Contact form submission: $name";
+	$email_subject = "[RealEval]: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	" Here are the details:\n Name: $name \n Organization: $org \n Email: $email_address \n Message \n $message"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
